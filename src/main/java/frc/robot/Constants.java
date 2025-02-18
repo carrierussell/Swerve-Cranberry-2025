281fd27cea68463c70702d6b844e53c5c029b7a6
Carrie Russell <77681170+carrierussell@users.noreply.github.com>
2025-02-18 00:02:42 -0500

    Base

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,233 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
+public class Constants {
+  public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(23.5);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(28.5);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0;
+    public static final double kBackLeftChassisAngularOffset = Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    // SPARK MAX CAN IDs
+    public static final int kFrontLeftDrivingCanId = 11;
+    public static final int kRearLeftDrivingCanId = 13;
+    public static final int kFrontRightDrivingCanId = 15;
+    public static final int kRearRightDrivingCanId = 17;
+
+    public static final int kFrontLeftTurningCanId = 10;
+    public static final int kRearLeftTurningCanId = 12;
+    public static final int kFrontRightTurningCanId = 16;
+    public static final int kRearRightTurningCanId = 14;
+
+    public static final boolean kGyroReversed = false;
+  }
+
+  public static final class ModuleConstants {
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+    // more teeth will result in a robot that drives faster).
+    public static final int kDrivingMotorPinionTeeth = 14;
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
+    public static final double kWheelDiameterMeters = 0.0762;
+    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
+    public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+    public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+        / kDrivingMotorReduction;
+  }
+  
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.05;
+    public static final int kOperatorControllerPort = 1;
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profiled robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+
+    public static final class NeoMotorConstants {
+    public static final double kFreeSpeedRpm = 5676;
+  }
+
+  public static final class Climber{
+    public static final int kClimberMotorId = 30;
+  }
+
+  public static class Elevator {
+    public static final int kElevatorLeftMotorId = 9;
+    public static final int kElevatorRightMotorId = 10;
+
+    public static final double kP = 0.15;
+    public static final double kI = 0;
+    public static final double kD = 0.0;
+    public static final double kIZone = 5.0;
+    public static final double kG = 0.5;
+
+    public static final double kMaxVelocity = 65;
+    public static final double kMaxAcceleration = 200;
+
+    public static final int kMaxCurrent = 40;
+
+    public static final double kStowHeight = 0.0;
+    public static final double kL2Height = 9.0;
+    public static final double kL3Height = 25.14;
+    public static final double kL4Height = 52.0;
+    public static final double kMaxHeight = 56.2;
+    public static final double kGroundAlgaeHeight = 0.0;
+    public static final double kScoreAlgaeHeight = 0.0;
+    public static final double kLowAlgaeHeight = 24.8;
+    public static final double kHighAlgaeHeight = 42.5;
+  }
+
+  public static class Coral {
+    public static final int kLeftMotorId = 4;  //updated to match our architecture
+    public static final int kRightMotorId = 5; //uodated to match our architecture
+
+    public static final int kLaserId = 3;  //updated to match our architecture
+    public static final int kColorId = 2;  //updated to match our architecture
+
+    public static final double kMaxCurrent = 20;
+
+    public static final double kP = 0.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+    public static final double kIZone = 0;
+
+    public static final double kIntakeSpeed = 0.3;
+    public static final double kReverseSpeed = -0.3;
+    public static final double kL1Speed = 0.4;
+    public static final double kL24Speed = 0.4;
+    public static final double kIndexSpeed = 0.1;
+    public static final double kSpeedDifference = kL1Speed * 0.5;
+  }
+
+  public static class Algae {
+    // WRIST
+    public static final int kWristMotorId = 7;       //updated to match our architecture
+    public static final int kIntakeMotorId = 6;     //updated to match our architecture
+
+    public static final int kWristEncoderId = 5;     //updated to match our architecture
+
+    public static final int kMaxWristCurrent = 10;  
+
+    public static final double kWristP = 0.01;
+    public static final double kWristI = 0.0;
+    public static final double kWristD = 0.0;
+
+    public static final double kWristKS = 0.0;
+    public static final double kWristKG = 0.0;
+    public static final double kWristKV = 0.100;
+    public static final double kWristKA = 0.0;
+
+    public static final double kWristOffset = 141.0;
+
+    public static final double kWristMaxVelocity = 690.0;
+    public static final double kWristMaxAcceleration = 1380.0;
+
+    public static final double kStowAngle = 233.0;
+    public static final double kDeAlgaeAngle = 215.0;
+    public static final double kGroundIntakeAngle = 162.0;
+
+    // INTAKE
+    public static final int kMaxIntakeCurrent = 20;
+
+    public static final double kIntakeSpeed = 0.6;
+    public static final double kEjectSpeed = -0.3;
+    public static final double kGroundIntakeSpeed = -0.3;
+  }
+
+  /*public static class Intake {
+    // Motors
+    public static final int kIntakeMotorId = 9;
+    public static final int kPivotMotorId = 10;
+
+    // DIO
+    public static final int k_pivotEncoderId = 0;
+    public static final int k_intakeLimitSwitchId = 2;
+
+    // Absolute encoder offset
+    public static final double k_pivotEncoderOffset = 0.166842; // Straight up, sketchy to reset to "up"
+
+    // Pivot set point angles
+    public static final double k_pivotAngleGround = 60;
+    public static final double k_pivotAngleSource = 190;
+    public static final double k_pivotAngleAmp = k_pivotAngleSource;
+    public static final double k_pivotAngleStow = 275;
+
+    // Intake speeds
+    public static final double k_intakeSpeed = 0.7;
+    public static final double k_ejectSpeed = -0.45;
+    public static final double k_feedShooterSpeed = -0.5;
+  }*/
+
+  // DIO
+
+  /*  Drivetrain
+  public static class Drive {
+    public static final double kP = 0.0; // 0.00085;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+
+    public static final double kS = 0.1695;// 0.01;
+    public static final double kV = 2.8559;// 2.6;
+    public static final double kA = 0.4864;
+
+    public static final int kFLMotorId = 8;
+    public static final int kBLMotorId = 7;
+    public static final int kFRMotorId = 6;
+    public static final int kBRMotorId = 5;
+}
+  */
+
+  public static class Field {
+    public static final double k_width = Units.feetToMeters(54.0);
+    public static final double k_length = Units.feetToMeters(27.0);
+
+    public static final Pose2d redCenterPose2d = new Pose2d(15.19, 5.50, new Rotation2d(Units.degreesToRadians(180.0)));
+    public static final Pose2d blueCenterPose2d = new Pose2d(1.27, 5.50, new Rotation2d(0));
+  }
+
+  public static class LEDs {
+    public static final int k_PWMId = 9;
+    public static final int k_totalLength = 300;
+  }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,278 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+//import frc.robot.autonomous.AutoRunner;
+//import frc.robot.autonomous.tasks.Task;
+import frc.robot.controls.controllers.DriverController;
+import frc.robot.controls.controllers.OperatorController;
+import frc.robot.simulation.Field;
+import frc.robot.subsystems.Algae;
+import frc.robot.subsystems.Coral;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Subsystem;
+import frc.robot.subsystems.leds.LEDs;
+import frc.robot.Constants;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
+ */
+public class Robot extends LoggedRobot {
+  // Controller
+  private final DriverController m_driverController = new DriverController(0, true, true);
+  private final OperatorController m_operatorController = new OperatorController(1, true, true);
+  private final GenericHID sysIdController = new GenericHID(2);
+
+  // private final SlewRateLimiter m_speedLimiter = new
+  // SlewRateLimiter(Drivetrain.kMaxAcceleration);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(Math.PI * 8);
+
+  // Robot subsystems
+  private List<Subsystem> m_allSubsystems = new ArrayList<>();
+  private final DriveSubsystem m_drive = new DriveSubsystem();
+  private final Coral m_coral = Coral.getInstance();
+  private final Algae m_algae = Algae.getInstance();
+  private final Elevator m_elevator = Elevator.getInstance();
+
+  public final LEDs m_leds = LEDs.getInstance();
+
+  // Auto stuff
+ // private Task m_currentTask;
+  //private AutoRunner m_autoRunner = AutoRunner.getInstance();
+
+  // Simulation stuff
+  private final Field m_field = Field.getInstance();
+
+  /**
+   * This function is run when the robot is first started up.
+   */
+  @Override
+  public void robotInit() {
+    CameraServer.startAutomaticCapture();
+
+    setupLogging();
+
+    // Add all subsystems to the list
+    //m_allSubsystems.add(m_drive);
+    m_allSubsystems.add(m_coral);
+    m_allSubsystems.add(m_algae);
+    m_allSubsystems.add(m_elevator);
+
+    m_allSubsystems.add(m_leds);
+
+    // Set up the Field2d object for simulation
+    SmartDashboard.putData("Field", m_field);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    m_allSubsystems.forEach(subsystem -> subsystem.periodic());
+    m_allSubsystems.forEach(subsystem -> subsystem.writePeriodicOutputs());
+    m_allSubsystems.forEach(subsystem -> subsystem.outputTelemetry());
+    m_allSubsystems.forEach(subsystem -> subsystem.writeToLog());
+
+    updateSim();
+
+    // Used by sysid
+    if (this.isTestEnabled()) {
+      CommandScheduler.getInstance().run();
+    }
+  }
+
+  @Override
+  public void autonomousInit() {
+   /*  m_currentTask = m_autoRunner.getNextTask();
+
+    // Start the first task
+    if (m_currentTask != null) {
+      m_currentTask.start();
+    }*/
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    /*  If there is a current task, run it
+    if (m_currentTask != null) {
+      // Run the current task
+      m_currentTask.update();
+      m_currentTask.updateSim();
+
+      // If the current task is finished, get the next task
+      if (m_currentTask.isFinished()) {
+        m_currentTask.done();
+        m_currentTask = m_autoRunner.getNextTask();
+
+        // Start the next task
+        if (m_currentTask != null) {
+          m_currentTask.start();
+        }
+      }
+    }*/
+  }
+
+  @Override
+  public void teleopInit() {
+    m_leds.breathe();
+  }
+
+  double speed = 0;
+  boolean scorePressed = false;
+
+  @Override
+  public void teleopPeriodic() {
+    m_drive.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), 0.05),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), 0.05),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), 0.05),
+                true);
+
+    // FINAL DRIVER CONTROLS
+    if (m_driverController.getWantsScoreCoral()) {
+      scorePressed = true;
+
+      if (m_elevator.getState() == Elevator.ElevatorState.STOW) {
+        m_coral.scoreL1();
+      } else {
+        m_coral.scoreL24();
+      }
+    } else if (scorePressed) {
+      scorePressed = false;
+
+      m_elevator.goToElevatorStow();
+      m_coral.intake();
+    } else if (m_driverController.getWantsScoreAlgae()) {
+      m_algae.score();
+    } else if (m_driverController.getWantsGroundAlgae()) {
+      m_algae.groundIntake();
+    }
+
+    // FINAL OPERATOR CONTROLS
+    if (m_operatorController.getWantsElevatorStow()) {
+      m_elevator.goToElevatorStow();
+      m_algae.stow();
+    } else if (m_operatorController.getWantsElevatorL2()) {
+      m_elevator.goToElevatorL2();
+      m_algae.stow();
+    } else if (m_operatorController.getWantsElevatorL3()) {
+      m_elevator.goToElevatorL3();
+      m_algae.stow();
+    } else if (m_operatorController.getWantsElevatorL4()) {
+      m_elevator.goToElevatorL4();
+      m_algae.stow();
+    } else if (m_operatorController.getWantsA1()) {
+      m_elevator.goToAlgaeLow();
+      m_algae.grabAlgae();
+    } else if (m_operatorController.getWantsA2()) {
+      m_elevator.goToAlgaeHigh();
+      m_algae.grabAlgae();
+    } else if (m_operatorController.getWantsStopAlgae()) {
+      m_algae.stopAlgae();
+      m_algae.stow();
+    } else if (m_operatorController.getWantsGroundAlgae()) {
+      m_algae.groundIntake();
+    } else if (m_operatorController.getWantsCoralIntake()) {
+      m_coral.intake();
+    }
+
+    // if (m_driverController.getWantsScoreCoral()) {
+    // if (m_elevator.getState() == Elevator.ElevatorState.STOW) {
+    // m_coral.scoreL1();
+    // } else {
+    // m_coral.scoreL24();
+    // }
+    // } else if (m_driverController.getWantsIntakeCoral()) {
+    // m_coral.intake();
+    // m_elevator.goToElevatorStow();
+    // }
+
+    // if (m_operatorController.getWantsElevatorReset() ||
+    // m_driverController.getWantsElevatorReset()) {
+    // RobotTelemetry.print("Resetting elevator");
+    // m_elevator.reset();
+    // }
+  }
+
+  @Override
+  public void disabledInit() {
+    m_leds.rainbow();
+    // m_leds.setColor(Color.kRed);
+
+    speed = 0;
+    m_allSubsystems.forEach(subsystem -> subsystem.stop());
+
+    // TODO: reset the auto state stuff if we're in dev mode
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void disabledExit() {
+  }
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {
+    
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+
+  private void updateSim() {
+    // Update the odometry in the sim.
+    m_field.setRobotPose(m_drive.getPose());
+  }
+
+  @SuppressWarnings("resource")
+  private void setupLogging() {
+    Logger.recordMetadata("ProjectName", "Flipside"); // Set a metadata value
+
+    if (isReal()) {
+      new WPILOGWriter(); // Log to the RoboRIO
+
+      // TODO: Add the next line back with a USB stick
+      // Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+      Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+      new PowerDistribution(1, ModuleType.kCTRE); // Enables power distribution logging
+    }
+
+    Logger.start();
+  }
+}
